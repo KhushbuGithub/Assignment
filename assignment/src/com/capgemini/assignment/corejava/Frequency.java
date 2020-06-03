2d46b5709a3ad475a35430157b23420626f6ee48
KhushbuGithub <khushbukumari3031@gmail.com>
2020-06-04 01:00:18 +0530

    Java Assignmnt

---
+++ assignment/src/com/capgemini/assignment/corejava/Frequency.java
@@ -0,0 +1,12 @@
+package com.capgemini.assignment.corejava;
+
+public class Frequency {
+	static int[] arr= new int[] {1,2,3,3,4,4,4,4,6,66,6,1};
+	public static void test() {
+		
+	}
+	public static void main(String[] args) {
+		test();
+	}
+
+}
